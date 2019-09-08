55778289884e0e66d52f504c65ca03031b167f57
Richard <25515721+RichardMoj@users.noreply.github.com>
2019-09-07 17:22:15 -0700

    Read instructions better

--- lab2.hs
+++ lab2.hs
@@ -133,8 +133,8 @@ rst_test = refl rst && symm rst && trans rst
 
 -- refl, symm, not trans
 rst' :: Reln
-rst' = [(1,1), (2,2), (2,3), (3,2)]
-rst'_test = refl rst'
+rst' = [(1,1), (2,2), (3,3), (4,4), (5,5), (6,6), (7,7), (8,8), (2,3), (3,2), (3,4), (4,3)]
+rst'_test = not(trans rst') && (refl rst' && symm rst')
 
 -- refl, not symm, trans
 rs't :: Reln
