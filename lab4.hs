e44ebeca4cbbaa190ac0b0d69bcae510e522cce8
Richard <25515721+RichardMoj@users.noreply.github.com>
2019-09-29 16:55:58 -0700

    Finished!

--- lab4.hs
+++ lab4.hs
@@ -1,4 +1,8 @@
 -- CSci 119, Lab 4
+-- Richard Mojarro
+-- CSCI 119
+-- 2019FA
+
 
 import Data.List (sort, stripPrefix) -- for your solution to Lab 3
 import Control.Monad (replicateM)    -- for strings function at the end
@@ -78,6 +82,75 @@ toRE w = go w [] where
 -- After a few days, I will release my solution for you to use. Don't
 -- duplicate the code just given above.
 
+-- Cartesian product, preserves normalization
+cart :: [a] -> [b] -> [(a,b)]
+cart xs ys = [(x,y) | x <- xs, y <- ys]
+
+-- Powerset, preserves normalization
+power :: [a] -> [[a]]
+power [] = [[]]
+power (x:xs) = let ys = power xs
+               in [] : map (x:) ys ++ tail ys
+
+-- Concatenation of LOLs, preserves invariant
+dot :: LOL a -> LOL a -> LOL a
+dot xs (LOL 0 _) = xs   -- Not needed, but makes kstar more efficient
+dot (LOL n xs) (LOL m ys) = LOL (n+m) (xs++ys)
+
+-- Reverse of LOLs, preserves invariant
+rev :: LOL a -> LOL a
+rev (LOL n xs) = LOL n (reverse xs)
+
+-- Merge of langages (aka "union")
+merge :: Ord a => Lang a -> Lang a -> Lang a
+merge [] ys = ys
+merge xs [] = xs
+merge xs@(x:xr) ys@(y:yr) =
+  case compare x y of
+    LT -> x : merge xr ys
+    EQ -> x : merge xr yr
+    GT -> y : merge xs yr
+
+-- Concatenation of languages
+cat :: Ord a => Lang a -> Lang a -> Lang a
+cat [] _ = []
+cat _ [] = []
+cat (x:xr) ys@(y:yr) = dot x y : merge (map (dot x) yr) (cat xr ys)
+
+-- Left quotient of a language by a string
+leftq :: Ord a => LOL a -> Lang a -> Lang a
+leftq _ [] = []
+leftq w@(LOL n xs) (LOL m ys:yss) =
+  case stripPrefix xs ys of
+    Nothing -> leftq w yss
+    Just zs -> LOL (m-n) zs : leftq w yss
+
+-- Kleene star of languages
+kstar :: Ord a => Lang a -> Lang a
+kstar [] = [eps]
+kstar (LOL 0 []:xr) = kstar xr 
+kstar xs = eps : cat xs (kstar xs)
+
+-- The language associated to a regular expression, i.e., [[r]]
+lang_of :: RegExp -> Lang Char
+lang_of Empty = []
+lang_of (Let a) = lang [[a]]
+lang_of (Union r1 r2) = merge (lang_of r1) (lang_of r2)
+lang_of (Cat r1 r2) = cat (lang_of r1) (lang_of r2)
+lang_of (Star r1) = kstar (lang_of r1)
+
+-- The one-string and finite languages of Theorem 3.2.
+onestr :: String -> RegExp
+onestr [] = Star Empty
+onestr [x] = Let x
+onestr (x:xs) = Cat (Let x) (onestr xs)
+
+finite :: [String] -> RegExp
+finite [] = Empty
+finite [w] = onestr w
+finite (w:ws) = Union (onestr w) (finite ws)
+
+
 
 ---------------- Part 1 ----------------
 
@@ -108,7 +181,7 @@ empty (Star r1) = False
 unitary :: RegExp -> Bool
 unitary (Empty) = False
 unitary (Let a) = False
-unitary (Union r1 r2) = (unitary r1 && empty r2) || (empty r2 && unitary r1) || (unitary r1 && unitary r2)
+unitary (Union r1 r2) = (unitary r1 && empty r2) || (empty r1 && unitary r2) || (unitary r1 && unitary r2)
 unitary (Cat r1 r2) = unitary r1 && unitary r2
 unitary (Star r1) = empty r1 || unitary r1
 
@@ -174,6 +247,139 @@ False
 False
 
 
+empty
+
+*Main> empty (Union (Empty) (Let 'a'))
+False
+*Main> empty (Union (Let 'b') (Let 'a'))
+False
+*Main> 
+*Main> empty (Union Empty Empty)
+True
+*Main> empty (Cat Empty (Let 'a'))
+True
+*Main> empty (Cat (Let 'b') (Let 'a'))
+False
+*Main> empty (Star Empty)
+False
+
+
+union
+
+*Main> unitary Empty
+False
+*Main> unitary (Let 'a')
+False
+*Main> unitary (Union (Empty) (Empty))
+False
+*Main> unitary (Union (Star Empty) (Empty))
+True
+*Main> unitary (Cat (Star Empty) (Star Empty))
+True
+*Main> unitary (Cat (Star Empty) (Let 'a'))
+False
+*Main> unitary (Star Empty)
+True
+*Main> unitary (Star (Cat (Star Empty) (Empty)))
+True
+*Main> unitary (Star (Cat (Star Empty) (Let 'a')))
+False
+
+
+byp
+
+*Main> byp (Empty)
+False
+*Main> byp (Let 'a')
+False
+*Main> byp (Union (Star Empty) (Let 'a'))
+True
+*Main> byp (Union (Let 'b') (Let 'a'))
+False
+*Main> byp (Cat (Star (Let 'b')) (Star (Let 'a')))
+True
+*Main> byp (Cat (Let 'b') (Star (Let 'a')))
+False
+*Main> byp (Star (Let 'a'))
+True
+
+
+inf
+
+*Main> inf Empty
+False
+*Main> inf (Let 'a')
+False
+*Main> inf (Union (Star (Let 'a')) (Let 'b'))
+True
+*Main> inf (Union (Let 'a') (Let 'b'))
+False
+*Main> inf (Cat (Star (Let 'a')) (Let 'b'))
+True
+*Main> inf (Cat (Star (Let 'a')) (Empty))
+False
+*Main> inf (Star (Let 'a'))
+True
+*Main> inf (Star (Empty))
+False
+
+
+rever
+*Main> rever Empty
+Empty
+*Main> rever (Let 'a')
+Let 'a'
+*Main> rever (Union (Let 'a') (Let 'b'))
+Union (Let 'a') (Let 'b')
+*Main> rever (Union (Let 'k') (Let 'a'))
+Union (Let 'k') (Let 'a')
+*Main> rever (Union (Cat (Let 'a') (Let 'b')) (Let 'c'))
+Union (Cat (Let 'b') (Let 'a')) (Let 'c')
+*Main> rever (Cat (Let 'b') (Let 'c'))
+Cat (Let 'c') (Let 'b')
+*Main> rever (Star (Let 'c'))
+Star (Let 'c')
+
+
+lq
+*Main> lq 'r' (Empty)
+Empty
+*Main> lq 'r' (Let 'r')
+Star Empty
+*Main> lq 'r' (Empty)
+Empty
+*Main> lq 'r' (Union (Let 'a') (Let 'b'))
+Union Empty Empty
+*Main> lq 'r' (Union (Let 'r') (Let 'b'))
+Union (Star Empty) Empty
+*Main> lq 'r' (Cat (Let 'a') (Let 'r'))
+Cat Empty (Let 'r')
+*Main> lq 'r' (Cat (Star (Let 'a')) (Let 'r'))
+Union (Cat (Cat Empty (Star (Let 'a'))) (Let 'r')) (Star Empty)
+*Main> lq 'r' (Star (Let 'a'))
+Cat Empty (Star (Let 'a'))
+*Main> lq 'r' (Star (Let 'r'))
+Cat (Star Empty) (Star (Let 'r'))
+
+
+nep
+*Main> nep Empty
+Empty
+*Main> nep (Let 'x')
+Let 'x'
+*Main> nep (Union (Let 'x') (Let 'b'))
+Union (Let 'x') (Let 'b')
+*Main> nep (Union (Empty) (Let 'b'))
+Union Empty (Let 'b')
+*Main> nep (Cat (Star (Let 'x')) (Let 'a'))
+Union (Cat (Cat (Let 'x') (Star (Let 'x'))) (Let 'a')) (Let 'a')
+*Main> nep (Cat (Let 'b') (Let 'a'))
+Cat (Let 'b') (Let 'a')
+*Main> nep (Star Empty)
+Cat Empty (Star Empty)
+
+
+
 -}
 
 
@@ -198,20 +404,19 @@ match1 (Star r1) w =  (null w) || or[(match1 r1 w1) && (match1 (Star r1) w2) | (
 
 
 
-
-
 -- Helper function
 match2' :: [RegExp] -> Bool -> String -> Bool
-match2' (r:rs) b w 
-  | null (r:rs) = False       --Empty list, dont care about b or w
-  | r == Empty = False        -- If head of the list is Empty
-  | otherwise = True
-
-
+match2' r b w | null r = null w
+match2' ((Empty):rs) b w = False
+match2' ((Let x):rs) b w 
+  | length w >= 1 = (Let x == toRE [head w]) && match2' rs False (tail(w))
+  | otherwise = False
+match2' ((Union r1 r2):rs) b w = match2' (r1:rs) b w || match2' (r2:rs) b w
+match2' ((Cat r1 r2):rs) b w = match2' (r1:r2:rs) b w || b && byp(r1) && match2'(r2:rs) True w
+match2' ((Star r1):rs) b w = not b && (match2' rs False w || match2' (r1:(Star r1):rs) True w)
 
 match2 :: RegExp -> String -> Bool
-match2 r w = undefined
-
+match2 r w = match2' (r:[]) False w
 
 
 
@@ -238,12 +443,84 @@ strings n = concat $ [replicateM i sigma | i <- [0..n]]
 
 {- Part 2 Testing
 
+splits
+
+*Main> splits "racecar"
+[("","racecar"),("r","acecar"),("ra","cecar"),("rac","ecar"),("race","car"),("racec","ar"),("raceca","r"),("racecar","")]
+*Main> splits "r"
+[("","r"),("r","")]
+*Main> splits "ran"
+[("","ran"),("r","an"),("ra","n"),("ran","")]
 
 
+match1
+
+*Main> match1 Empty "racecar"
+False
+*Main> match1 (Let 'r') "racecar"
+False
+*Main> match1 (Let 'r') "r"
+True
+*Main> match1 (Let 'r') ""
+False
+*Main> match1 (Union (Let 'a') (Let 'b') ) "race"
+False
+*Main> match1 (Union (Let 'a') (Let 'b') ) "a"
+True
+*Main> match1 (Union (Let 'a') (Let 'b') ) "ab"
+False
+*Main> match1 (Union (Let 'a') (Let 'b') ) "b"
+True
+*Main> match1 (Cat (Let 'a') (Let 'b')) "ab"
+True
+*Main> match1 (Cat (Let 'a') (Let 'b')) "abrace"
+False
+*Main> match1 (Cat (Let 'a') (Let 'b')) "race"
+False
+*Main> match1 (Cat (Let 'a') (Let 'b')) "a"
+False
+*Main> 
+*Main> match1 (Star (Let 'a')) "race"
+False
+*Main> match1 (Star (Let 'a')) "rrr"
+False
+*Main> match1 (Star (Let 'r')) "rrr"
+True
+*Main> match1 (Star (Let 'r')) ""
+True
 
 
-
-
-
-
+match2
+*Main> match2 Empty "ab"
+False
+*Main> match2 Empty ""
+False
+*Main> match2 (Let 'a') ""
+False
+*Main> match2 (Let 'a') "a"
+True
+*Main> match2 (Let 'a') "ab"
+False
+*Main> match2 (Union (Let 'a') (Let 'b')) "ab"
+False
+*Main> match2 (Union (Let 'a') (Let 'b')) "a"
+True
+*Main> match2 (Union (Let 'a') (Let 'b')) "b"
+True
+*Main> match2 (Cat (Let 'a') (Let 'b')) "ab"
+True
+*Main> match2 (Cat (Let 'a') (Let 'b')) "abab"
+False
+*Main> match2 (Cat (Let 'a') (Let 'b')) "a"
+False
+*Main> match2 (Cat (Let 'a') (Let 'b')) "b"
+False
+*Main> match2 (Star (Cat (Let 'a') (Let 'b'))) "ab"
+True
+*Main> match2 (Star (Cat (Let 'a') (Let 'b'))) "a"
+False
+*Main> match2 (Star (Cat (Let 'a') (Let 'b'))) "b"
+False
+*Main> match2 (Star (Cat (Let 'a') (Let 'b'))) "abab"
+True
 -}
